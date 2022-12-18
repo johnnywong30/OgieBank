ef7bf2c2b225a32989c87b075d15c520c29a4e13
Sophia Zuo <sophiasaur.zuo@gmail.com>
2022-12-18 10:25:41 -0500

    add dummy export button to budget

--- client/src/routes/Pages/Budget/index.js
+++ client/src/routes/Pages/Budget/index.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Heading, 
     Container, 
-    SimpleGrid, 
+    SimpleGrid,
+    Button,
+    Stack 
 } from '@chakra-ui/react'
+import { CSVLink, CSVDownload } from 'react-csv';
 
 import Chart from './Chart';
 import Expenses from './Expenses';
 import Overview from './Overview';
 import Spending from './Spending';
 
+
 const Budget = () => {
 
     return (
         <Container maxW={'7xl'} px="12" py="6">
-            <Heading as="h1">Budget</Heading>
+            <Stack direction={'row'} justifyContent={'space-between'}>
+                <Heading as="h1">Budget</Heading>
+                <CSVLink data={[['budget placeholder']]}>
+                    <Button>Export</Button>
+                </CSVLink>
+            </Stack>
             <SimpleGrid columns={[1, null, 2]} spacingX="6" spacingY="3">
                 <SimpleGrid columns={1} spacingX="6" spacingY="3">
                     <Overview />

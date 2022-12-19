a5a0f92e00869d47a36dd48dd3bdaa9b3a81f5cf
Maxwell Metzner <maxwell.metzner@gmail.com>
2022-12-19 14:54:06 -0500

    Finished donut chart fixes
    the graph fills in the box depending on window size, it should appear larger and update automatically both when the user logs in and when a new transaction is added.

--- client/src/routes/Pages/Overview/Debt.js
+++ client/src/routes/Pages/Overview/Debt.js
@@ -8,73 +8,22 @@ import {
     Text,
     Stack,
     SimpleGrid, 
+    Button
 } from '@chakra-ui/react'
 
 const Debt = () => {   
     const allTransactions = useSelector((state) => state.transactions.transactions)
     const now = new Date()
 
-    const getTransactions = (timePeriod) => {
-        let transactions = []
-        const lastWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7)
-        const lastMonth = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate())
-        const lastYear = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate())
-        switch (timePeriod) {
-            case 'week':
-                // set transactions to all transactions that are within the past week
-                for (let i = 0; i < allTransactions.length; i++) {
-                    // translate date in MM/DD/YYYY format to a date object
-                    let date = new Date(allTransactions[i].date)
-                    if (date > lastWeek) {
-                        transactions.push(allTransactions[i])
-                    }
-                }
-                break;
-            case 'month':
-                for (let i = 0; i < allTransactions.length; i++) {
-                    let date = new Date(allTransactions[i].date)
-                    if (date > lastMonth) {
-                        transactions.push(allTransactions[i])
-                    }
-                }
-                break;
-            case 'year':
-                for (let i = 0; i < allTransactions.length; i++) {
-                    let date = new Date(allTransactions[i].date)
-                    if (date > lastYear) {
-                        transactions.push(allTransactions[i])
-                    }
-                }
-                break;
-            case 'all':
-                transactions = allTransactions
-                break;
-            default:
-                transactions = allTransactions
-                break;
-        }
-        return transactions
-    }
-
-    // given an array of transactions, and either 'source' or 'category', return an array of objects with the name and amount of the source/category
-    const getStats = (transactions, type) => {
+    const getStats = (transactions) => {
         let stats = []
         let total = 0
         transactions.forEach(t => {
-            if (type === 'source') {
-                let source = stats.find(s => s.name === t.source)
-                if (source) {
-                    source.amount += t.amount
-                } else {
-                    stats.push({name: t.source, amount: t.amount})
-                }
-            } else if (type === 'category') {
-                let category = stats.find(s => s.name === t.category)
-                if (category) {
-                    category.amount += t.amount
-                } else {
-                    stats.push({name: t.category, amount: t.amount})
-                }
+            let category = stats.find(s => s.name === t.category)
+            if (category) {
+                category.amount += t.amount
+            } else {
+                stats.push({name: t.category, amount: t.amount})
             }
             total += t.amount
         })
@@ -90,35 +39,24 @@ const Debt = () => {
     }
 
     // Display the stats in a table
-    const [timePeriod, setTimePeriod] = useState('all')
-    const [sourcePercentages, setSourcePercentages] = useState([])
+    const [updateGraphs, setUpdateGraphs] = useState(false)
     const [categoryPercentages, setCategoryPercentages] = useState([])
     const [categoryTotal, setCategoryTotal] = useState(0)
     
     useEffect(() => {
-        const transactions = getTransactions(timePeriod)
-        const sourceStats = getStats(transactions, 'source')
-        const categoryStats = getStats(transactions, 'category')
-        const sourcePercentages = getPercentages(sourceStats.stats, sourceStats.total)
+        const transactions = allTransactions
+        const categoryStats = getStats(transactions)
         const categoryPercentages = getPercentages(categoryStats.stats, categoryStats.total)
-        setSourcePercentages(sourcePercentages)
         setCategoryPercentages(categoryPercentages)
         setCategoryTotal(categoryStats.total)
     }
-
-    , [timePeriod])
+    , [updateGraphs, allTransactions])
 
     // create the pie charts
     const [categorySeries, setCategorySeries] = useState([])
     const [categoryLabels, setCategoryLabels] = useState([])
 
     useEffect(() => {
-        const sourceSeries = []
-        const sourceLabels = []
-        sourcePercentages.forEach(s => {
-            sourceSeries.push(s.percentage)
-            sourceLabels.push(s.name)
-        })
         const categorySeries = []
         const categoryLabels = []
         categoryPercentages.forEach(c => {
@@ -127,7 +65,7 @@ const Debt = () => {
         })
         setCategorySeries(categorySeries)
         setCategoryLabels(categoryLabels)
-    }, [sourcePercentages, categoryPercentages])
+    }, [categoryPercentages])
       
       const categoryOptions = {
         chart: {
@@ -195,9 +133,9 @@ const Debt = () => {
         }]
         };
 
-    // On page load, setTimePeriod('week')
+    // On page load, refresh the graphs
     useEffect(() => {
-        setTimePeriod('all')
+        setUpdateGraphs(!updateGraphs)
     }, [])
 
     return (
@@ -227,37 +165,17 @@ const Debt = () => {
                         </Stack>
                     </Stack>
                 </SimpleGrid>
-                    <div style={{backgroundColor: '#f5f5f5', padding: '20px'}}>
-                    <div>
-                        <div style={{display: 'flex', justifyContent: 'center', backgroundColor: '#e0e0e0', padding: '10px'}}>
-                            <Text fontSize={'l'} fontWeight={800}>
-                                Choose a time period: 
-                            </Text>
-                            <div style={{width: '10px'}}></div>
-                            <button onClick={() => setTimePeriod('week')}>Week</button>
-                            <div style={{width: '10px'}}></div>
-                            <button onClick={() => setTimePeriod('month')}>Month</button>
-                            <div style={{width: '10px'}}></div>
-                            <button onClick={() => setTimePeriod('year')}>Year</button>
-                            <div style={{width: '10px'}}></div>
-                            <button onClick={() => setTimePeriod('all')}>All</button>
-                        </div>
-                    </div>
-                    <div>
-                        <div style={{display: 'flex', justifyContent: 'center'}}>
-                            {/* if categoryOptions or categorySeries is empty, display a message saying "No transactions found", otherwise render the chart */}
-                            {categoryOptions.labels.length === 0 ? 
-                                <Text fontSize={'l'} fontWeight={800}>No transactions found</Text> :
-                                <ReactApexChart 
-                                    options={categoryOptions} 
-                                    series={categorySeries} 
-                                    type="donut" 
-                                    width={500}
-                                />
-                            }
-                        </div>
-                    </div>
-                </div>
+                        {/* if categoryOptions or categorySeries is empty, display a message saying "No transactions found", otherwise render the chart */}
+                        {categoryOptions.labels.length === 0 ? 
+                            <Text fontSize={'l'} fontWeight={800}>No transactions found</Text> :
+                            <ReactApexChart 
+                                options={categoryOptions} 
+                                series={categorySeries} 
+                                type="donut" 
+                                // fit the size of the div 
+                                width="100%"
+                            />
+                        }
             </Box>
         </Box>
     )
